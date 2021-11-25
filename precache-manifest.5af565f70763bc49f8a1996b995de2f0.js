351ef3e5b5bfc41779237ba7f96971c5ff4a62b8
Caio César Amorim <caiocavalcante063@gmail.com>
2021-11-25 09:40:12 -0300

    Updates

---
+++ precache-manifest.5af565f70763bc49f8a1996b995de2f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6717112cfb6aa4181996ad3895b0006e",
+    "url": "/Trivia-React-Redux/index.html"
+  },
+  {
+    "revision": "9623b009181e189d3c45",
+    "url": "/Trivia-React-Redux/static/css/main.cbf7972b.chunk.css"
+  },
+  {
+    "revision": "173799274a4a5e9ccc86",
+    "url": "/Trivia-React-Redux/static/js/2.c192c280.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/Trivia-React-Redux/static/js/2.c192c280.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9623b009181e189d3c45",
+    "url": "/Trivia-React-Redux/static/js/main.2a90d13d.chunk.js"
+  },
+  {
+    "revision": "6eab2e074c7784c40973",
+    "url": "/Trivia-React-Redux/static/js/runtime-main.761c4377.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/Trivia-React-Redux/static/media/trivia.466d153e.png"
+  }
+]);
